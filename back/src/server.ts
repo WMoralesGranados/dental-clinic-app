10deb067c181ea1d3bdded1a7af6c5f35863dc17
William Morales <williamorggan@gmail.com>
2025-05-30 15:55:55 -0600

    fix: fix server instance with env var

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { Express } from "express";
+import router from "./routes/router";
 
-const app = express();
+const app: Express = express();
 
-
-
+app.use(express.json());
+app.use(router);
 
 
 export default app;
